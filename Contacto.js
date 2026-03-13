97fe83c48a76b624cda7229ad10706916af01a24
abustamanteo <abustamanteo@hotmail.es>
2026-03-13 16:50:45 +0100

    Editor básico de texto en Contacto
    además se ha pulsado alt + shift + F para ajustar correctamente el margen

--- Contacto.js
+++ Contacto.js
@@ -10,24 +10,55 @@ formulario.addEventListener("submit", (e) => {
     //para elimnar espacios en blanco innecesarios
     let nombreValor = nombre.value.trim();
     let emailValor = email.value.trim();
-    let mensajeValor = mensaje.value.trim();
+    let mensajeValor = editorMensaje.innerText.trim();
 
-    if(nombreValor === "" || nombreValor.length < 4){
+    if (nombreValor === "" || nombreValor.length < 4) {
         alert("Por favor, introduce tu nombre.");
         return;
     }
 
-    if(emailValor ==="" || !emailValor.includes("@")){
+    if (emailValor === "" || !emailValor.includes("@")) {
         alert("Por favor, introduce un email válido.")
         return;
     }
 
-    if(mensajeValor.length < 10){
+    if (mensajeValor.length < 10) {
         alert("El mensaje debe de tener al menos 10 carácteres.");
         return;
     }
 
+    /*busca el elemento input con el atributo y valor que tenga entre la llave*/
+    document.querySelector("input[name='mensaje']").value = editorMensaje.innerHTML;
+
     alert("Mensaje enviado correctamente. Gracias por tu colaboración ;)");
-    
+
     formulario.submit(); //si la función no se ha roto a medio proceso, se envía el formulario.
+});
+
+//Seleccionar estilo para el texto
+const editorMensaje = document.getElementById("editorMensaje");
+const botonNegrita = document.getElementById("btn-negrita");
+const botonCursiva = document.getElementById("btn-cursiva");
+const botonSubrayado = document.getElementById("btn-subrayado");
+
+editorMensaje.addEventListener("click", () => {
+    if (!editorMensaje.dataset.editado) {
+        editorMensaje.innerHTML = "";
+        editorMensaje.dataset.editado = "true";
+    }
+});
+
+/*excComand() es un metodo JS que permite ejecutar comandos de edicion dentro de un 
+elemento editable*/
+
+botonNegrita.addEventListener("click", () => {
+    document.execCommand("bold"); 
+});
+
+botonCursiva.addEventListener("click", () => {
+    document.execCommand("italic");
+});
+
+botonSubrayado.addEventListener("click", () => {
+    document.execCommand("underline");
 });
